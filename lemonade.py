aecdd10b23b1551dd5dec34631b82659796decfd
SlippyDev <86132517+SlippiDev@users.noreply.github.com>
2022-12-27 11:17:21 -0800

    basic

--- lemonade.py
+++ lemonade.py
@@ -5,3 +5,8 @@ weather = rand.choice(weather_list)
 print("Welcome to the lemonade game!")
 print("Day 1: ")
 print("Weather: {}".format(weather))
+
+print("How much would you like to charge for a lemonade?")
+lemonade_price = float(input("--> "))
+
+if lemonade_price >= 
